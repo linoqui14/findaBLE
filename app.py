091fca257d6001a78e8a9f34c439042c952eb0eb
Aquilino Cagubcub <56160486+linoqui14@users.noreply.github.com>
2023-01-26 18:34:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -404,6 +404,7 @@ def deleteESP32(id):
 
 @app.route("/update_esp32_mode/<id>/<mode>",methods=["GET","POST"])
 def updateESP32Mode(id,mode):
+    resetESPdistance()
     esp32PairDBJS.update_one({'id':id},{"$set":{'mode':int(mode),'reset':0}})
     return "1"
 
